9ebc5ac0986f1869d3027255b1f0c3a7d3913665
Shashwat Singh <shashwatsingh741@gmail.com>
2022-11-30 19:41:49 +0530

    Views.py configuered to accept akgec.ac.in domain emails only

--- registration_portal/views.py
+++ registration_portal/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 from django.views import View
 from django.contrib.auth.mixins import LoginRequiredMixin
+from social_django.models import UserSocialAuth
+from django.contrib.auth.models import User
+from django.contrib.auth import logout
 
 # Create your views here.
 class LandingPageView(View):
@@ -9,9 +13,19 @@ class LandingPageView(View):
 
 class RegistrationGoogleOAuthView(View):
     def get(self, request):
+        if request.user.is_authenticated:
+            logout(request)
         return render(request, 'registration_portal/google_oauth.html')
 
 class FillDetailsView(LoginRequiredMixin, View):
     def get(self, request):
-        return render(request, 'registration_portal/fill_details.html')
-        
+        email = request.user.email
+        domain = email.split("@")[-1]
+        entry_auth = UserSocialAuth.objects.filter(provider="google-oauth2").get(uid=email)
+        entry_user = User.objects.get(email = email)
+        if domain == 'akgec.ac.in':
+            return render(request, 'registration_portal/fill_details.html')
+        else:
+            entry_auth.delete()
+            entry_user.delete()
+            return redirect(reverse('registration_portal:landingpage'))
