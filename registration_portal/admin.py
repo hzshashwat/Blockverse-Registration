45047599c51e8bd81937abd6931778a0011a40ad
Shashwat Singh <shashwatsingh741@gmail.com>
2022-12-02 19:56:09 +0530

    Transaction model migration completed and registered to admin

--- registration_portal/admin.py
+++ registration_portal/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from registration_portal.models import Team
+from registration_portal.models import Team, Transaction
 
 # Register your models here.
 admin.site.register(Team)
+admin.site.register(Transaction)
